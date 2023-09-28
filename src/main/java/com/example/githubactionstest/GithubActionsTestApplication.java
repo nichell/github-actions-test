6c0907c4cf7e554589227afa28ddb97fb9700fbe
nichell.nicolas <nichell@gmx.de>
2023-09-28 12:23:08 +0200

    Second initial commit

---
+++ src/main/java/com/example/githubactionstest/GithubActionsTestApplication.java
@@ -0,0 +1,14 @@
+package com.example.githubactionstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsTestApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Application started");
+		SpringApplication.run(GithubActionsTestApplication.class, args);
+	}
+
+}
